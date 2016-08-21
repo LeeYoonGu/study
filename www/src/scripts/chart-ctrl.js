750bebd8a2a21fcb67dc6114a3eb0ef4a69700a3
엑소버드 <perfectacle@naver.com>
2016-08-21 14:34:49 +0900

    문서 보완

--- www/src/scripts/chart-ctrl.js
+++ www/src/scripts/chart-ctrl.js
@@ -2,7 +2,7 @@
 angular.module('myModule', ['chart.js'])
        .controller('ChartCtrl', ['$scope', function($scope) {
          $scope.children = {
-           labels: ['양간장', '모찌니'],
+           labels: ['양간장dddd', '모찌니'],
            series: ['잉여력', '코딱지'],
            data: [
              [99, 22],
